321cf25345e7e40f6de9c1f8db2eef80d1b195f0
raghu <raghu.sk@gmail.com>
2015-10-05 10:27:26 +0530

    reenabed gzip

--- src/sportzbee/middleware.clj
+++ src/sportzbee/middleware.clj
@@ -70,4 +70,5 @@
             (assoc-in [:security :anti-forgery] false)
             (dissoc :session)))
       wrap-context
-      wrap-internal-error))
+      wrap-internal-error
+      (wrap-gzip)))

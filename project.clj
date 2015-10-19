ce71e3feb9d805c0b943be2e19ff5a79c81daca1
raghu <raghu.sk@gmail.com>
2015-10-19 19:09:30 +0530

    Added references to datomci pro libraries

--- project.clj
+++ project.clj
@@ -105,6 +105,9 @@
                         :port       3001
                         :nrepl-port 7001}}
    :repositories {"my.datomic.com" {:url "https://my.datomic.com/repo"
+                                 :username ~(System/getenv "DATOMIC_EMAIL")
+                      :password ~(System/getenv "DATOMIC_KEY")}}
+   :repositories1 {"my.datomic.com" {:url "https://my.datomic.com/repo"
                                  :username "raghu.sk@gmail.com"
                       :password "68600bf7-cc9c-4062-8c90-386c78034630"}}
    :profiles/dev {}
